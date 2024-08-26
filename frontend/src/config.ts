0f259857c814ada89338d732cc7eb1ec7dbc7187
FrinsGupta <pryncegupta@gmail.com>
2024-08-26 22:56:13 +0530

    Fixed Backend Url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,1 @@
-// export const BackendUrl = "https://backend.pryncegupta.workers.dev"
-export const BackendUrl = "http://127.0.0.1:8787"
+export const BackendUrl = "https://backend.pryncegupta.workers.dev"
